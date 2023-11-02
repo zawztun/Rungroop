3ed6bfd03820eefe3a1fece75e11cf95fbf29a63
zawztun <zzt.nyc@gmail.com>
2023-11-02 11:31:00 -0400

    create_Event Entity and EventDto

--- src/main/java/com/rungroop/web/models/Event.java
+++ src/main/java/com/rungroop/web/models/Event.java
@@ -13,23 +13,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+
 @Entity
-@Table(name = "clubs")
-public class Club {
+public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
-    private String title;
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String Type;
     private String photoUrl;
-    private String content;
     @CreationTimestamp
     private LocalDateTime createdOn;
     @UpdateTimestamp
     private LocalDateTime updatedOn;
 
-    @OneToMany(mappedBy = "club", cascade = CascadeType.REMOVE)
-    private Set<Event> events = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name="club_id", nullable = false)
+    private Club club;
+
 }
